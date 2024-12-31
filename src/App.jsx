9da9f055910e5b025c91b5921f77c0559b93d208
badshakn <badshanoordeen@gmail.com>
2024-12-31 23:09:42 +0530

    icon 2nd

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
 
   return (
     <>
-      <Icons />
       <span>still no reflect any automation </span>
+      <Icons />
       <h1>really excited live develop environment </h1>
       <p>now from container file ,it has been changed by local file too</p>
       <h3>CI-CD DEPLOYMENT</h3>
