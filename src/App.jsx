574ffc469117106a8ab50843f3261bd5a606f5da
badshakn <badshanoordeen@gmail.com>
2024-12-31 21:47:24 +0530

    up icons

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,14 @@ function App() {
 
   return (
     <>
+      <Icons />
       <span>still no reflect any automation </span>
       <h1>really excited live develop environment </h1>
       <p>now from container file ,it has been changed by local file too</p>
-      <Icons />
       <h3>CI-CD DEPLOYMENT</h3>
     </>
   );
 }
 
 export default App;
+// docker push badshanoordeen/just-try:latest
