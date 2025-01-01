85a7dbcf4c409148ad5e8c3fd701df0f7f6def27
badshakn <badshanoordeen@gmail.com>
2025-01-01 17:48:46 +0530

    icon to last

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
 
   return (
     <>
-      <Icons />
       <span>still no reflect any automation </span>
       <h1>really excited live develop environment </h1>
       <p>now from container file ,it has been changed by local file too</p>
       <h3>CI-CD DEPLOYMENT</h3>
+      <Icons />
     </>
   );
 }
